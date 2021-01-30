305346bd746e115c75dc436077a50676f084d317
T Sathya Narayana <61154612+sathya050801@users.noreply.github.com>
2021-01-30 13:17:40 +0530

    Add files via upload

---
+++ Searching/Linear Search.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+   if(key == arr[i])
+     int main()
+{
+    int arr[10],i,num,key;
+    cout<<"Enter the no. of elements: ";
+    cin>>num;
+    cout<<"\n";
+    
+    cout<<"Enter the elements: ";
+    for(i=0;i<num;i++)
+    {
+        cin>>arr[i];
+        cout<<"\t";
+    }
+    cout<<endl;
+    key=5;
+    
+    for(i=0;i<num;i++)
+    {
+        cout<<"The position is "<<i<<endl;
+    }
+
+    return -1;
+}
+
